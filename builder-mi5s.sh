35c8ee9fad71e9adda18cfda32f3c084757935dc
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2018-11-03 12:08:44 +0100

    Kang only github builder from Pepsy

---
+++ builder-mi5s.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+KERNEL_DIR=$PWD
+ANYKERNEL_DIR=$KERNEL_DIR/AnyKernel2
+CCACHEDIR=../CCACHE/capricorn
+TOOLCHAINDIR=/pipeline/build/root/toolchain/aarch64-linux-android-4.9
+DATE=$(date +"%d%m%Y")
+KERNEL_NAME="Syberia"
+DEVICE="-capricorn-"
+VER="-0.1"
+TYPE="PIE-EAS"
+FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$TYPE""$VER".zip
+
+rm $ANYKERNEL_DIR/capricorn/Image.gz-dtb
+rm $KERNEL_DIR/arch/arm64/boot/Image.gz $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb
+
+export ARCH=arm64
+export KBUILD_BUILD_USER="mesziman"
+export KBUILD_BUILD_HOST="github"
+export CC=/pipeline/build/root/toolchain/SnapDragonLLVM_6.0/prebuilt/linux-x86_64/bin/clang
+export CXX=/pipeline/build/root/toolchain/SnapDragonLLVM_6.0/prebuilt/linux-x86_64/bin/clang++
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=$TOOLCHAINDIR/bin/aarch64-linux-android-
+export LD_LIBRARY_PATH=$TOOLCHAINDIR/lib/
+export USE_CCACHE=1
+export CCACHE_DIR=$CCACHEDIR/.ccache
+
+make clean && make mrproper
+make capricorn_defconfig
+make -j$( nproc --all )
+
+{
+cp $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb $ANYKERNEL_DIR/capricorn
+}
+
+
+cd $ANYKERNEL_DIR/capricorn
+zip -r9 $FINAL_ZIP * -x *.zip $FINAL_ZIP
+mv $FINAL_ZIP /pipeline/output/$FINAL_ZIP
