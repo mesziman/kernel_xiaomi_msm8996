2fce67eb1e3df0b633d1cf26ca70c118968a4a2c
Gabor <meszaros.gabor.10k@gmail.com>
2018-11-18 19:31:07 +0100

    Update wercker.sh

--- wercker.sh
+++ wercker.sh
@@ -5,5 +5,5 @@ dpkg --add-architecture i386 && apt-get update && apt-get install -y git ccache 
 git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 /pipeline/build/root/toolchain/aarch64-linux-android-4.9 &&
 git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 /pipeline/build/root/toolchain/arm-linux-androideabi-4.9 &&
 git clone https://github.com/PsyMan47/SnapDragonLLVM_6.0 /pipeline/build/root/toolchain/SnapDragonLLVM_6.0
-
+git clone -b 8.0 https://github.com/syberia-project/DragonTC /pipeline/build/root/toolchain/dtc
 bash builder-mi5s.sh
