60c95d5ced1f45d3fb27d9e64e16049c4fb7cb7e
Bruce Wayne <bruce.wayne@wayneenterprises.com>
2019-03-05 09:34:41 +0000

    googleclang
    Conflicts:
    	Makefile
    
    Conflicts:
    	Makefile
    	builder-mi5s.sh

--- builder-mi5s.sh
+++ builder-mi5s.sh
@@ -14,15 +14,18 @@ FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$TYPE""$VER".zip
 
 rm $ANYKERNEL_DIR/capricorn/Image.gz-dtb
 rm $KERNEL_DIR/arch/arm64/boot/Image.gz $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb
-
+#PATH="/pipeline/build/root/toolchain/gclang/clang-r349610/bi
+PATH="${PATH}:${TOOLCHAINDIR}/bin:${TOOLCHAIN32}/bin"
 export ARCH=arm64
 export KBUILD_BUILD_USER="mesziman"
 export KBUILD_BUILD_HOST="github"
-export CC=/pipeline/build/root/toolchain/SnapDragonLLVM_6.0/prebuilt/linux-x86_64/bin/clang
-export CXX=/pipeline/build/root/toolchain/SnapDragonLLVM_6.0/prebuilt/linux-x86_64/bin/clang++
+#export CC=/pipeline/build/root/toolchain/dtc/bin/clang
+#export CC=/pipeline/build/root/toolchain/gclang/clang-r349610/bin
+#export CXX=/pipeline/build/root/toolchain/dtc/bin/clang++
+export CC=clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
-export CROSS_COMPILE=$TOOLCHAINDIR/bin/aarch64-linux-android-
-export CROSS_COMPILE32=$TOOLCHAINDIR/bin/arm-linux-androideabi-
+export CROSS_COMPILE=aarch64-linux-android-
+export CROSS_COMPILE32=arm-linux-androideabi-
 export LD_LIBRARY_PATH=$TOOLCHAINDIR/lib/
 export USE_CCACHE=1
 export CCACHE_DIR=$CCACHEDIR/.ccache
