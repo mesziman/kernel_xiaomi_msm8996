efae7619637e5804863a7168b71f8f0d194e2912
Gabor Meszaros <gabor.meszaros@nokia.com>
2019-03-06 12:02:54 +0000

    again wercker

--- wercker.sh
+++ wercker.sh
@@ -2,6 +2,7 @@
 
 
 dpkg --add-architecture i386 && apt-get update && apt-get install -y git ccache automake bc lzop bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 g++-multilib python-networkx libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng  libssl-dev &&
+export LOFASZ=$PWD &&
 git clone --depth=2 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 /pipeline/build/root/toolchain/aarch64-linux-android-4.9 &&
 cd /pipeline/build/root/toolchain/aarch64-linux-android-4.9 && git reset --hard 22f053ccdfd0d73aafcceff3419a5fe3c01e878b &&
 git clone --depth=2 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 /pipeline/build/root/toolchain/arm-linux-androideabi-4.9 &&
@@ -10,5 +11,5 @@ git clone --depth=2 https://android.googlesource.com/platform/prebuilts/clang/ho
 git clone --depth=2 https://github.com/PsyMan47/SnapDragonLLVM_6.0 /pipeline/build/root/toolchain/SnapDragonLLVM_6.0 &&
 #git clone --depth=2 -b 9.0 https://github.com/syberia-project/platform_prebuilts_build-tools /pipeline/build/root/toolchain/asd
 git clone --depth=2 -b 9.0 https://github.com/syberia-project/DragonTC /pipeline/build/root/toolchain/dtc 
-cd /
+cd $LOFASZ
 bash builder-mi5s.sh
