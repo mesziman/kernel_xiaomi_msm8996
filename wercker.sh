77d72502f359ada2776e62d0c9f92ac3d7b44395
Gabor <meszaros.gabor.10k@gmail.com>
2019-03-07 13:43:22 +0100

    wercker: silence apt-get install packages

--- wercker.sh
+++ wercker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-dpkg --add-architecture i386 && apt-get update && apt-get install -y git ccache automake bc lzop bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 g++-multilib python-networkx libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng  libssl-dev &&
+dpkg --add-architecture i386 && apt-get -qq update && apt-get -qq install -y git ccache automake bc lzop bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 g++-multilib python-networkx libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng  libssl-dev &&
 export LOFASZ=$PWD &&
 git clone --depth=2 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 /pipeline/build/root/toolchain/aarch64-linux-android-4.9 &&
 cd /pipeline/build/root/toolchain/aarch64-linux-android-4.9 && git reset --hard 22f053ccdfd0d73aafcceff3419a5fe3c01e878b &&
