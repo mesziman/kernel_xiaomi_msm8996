LineageOS/android_kernel_xiaomi_msm8996

input: fingerprint: fpc1020: Allow filtering of key events
 * Implement a key handler with the simple purpose of filtering
   the key events sent by the fingerprint HAL.

Change-Id: I114493e39b61df9307fea584dd68391698f320f3

drivers/input/fingerprint/fpc1020_tee.c
@@ -50,6 +50,8 @@
 #include <linux/fb.h>
 #endif
 
+#define FPC1020_NAME "fpc1020"
+
 #define FPC1020_RESET_LOW_US 1000
 #define FPC1020_RESET_HIGH1_US 100
 #define FPC1020_RESET_HIGH2_US 1250
@@ -76,6 +78,9 @@ struct fpc1020_data {
 	bool screen_on;
 	int proximity_state; /* 0:far 1:near */
 	spinlock_t irq_lock;
+
+	struct input_handler input_handler;
+	bool report_keys;
 };
 
 enum {
@@ -83,6 +88,65 @@ enum {
 	FP_ID_HIGH_1,
 	FP_ID_FLOAT_2,
 	FP_ID_UNKNOWN
+};
+
+static int input_connect(struct input_handler *handler,
+		struct input_dev *dev, const struct input_device_id *id) {
+	int rc;
+	struct input_handle *handle;
+	struct fpc1020_data *fpc1020 =
+		container_of(handler, struct fpc1020_data, input_handler);
+
+	if (!strstr(dev->name, "uinput-fpc"))
+		return -ENODEV;
+
+	handle = kzalloc(sizeof(struct input_handle), GFP_KERNEL);
+	if (!handle)
+		return -ENOMEM;
+
+	handle->dev = dev;
+	handle->handler = handler;
+	handle->name = FPC1020_NAME;
+	handle->private = fpc1020;
+
+	rc = input_register_handle(handle);
+	if (rc)
+		goto err_input_register_handle;
+
+	rc = input_open_device(handle);
+	if (rc)
+		goto err_input_open_device;
+
+	return 0;
+
+err_input_open_device:
+	input_unregister_handle(handle);
+err_input_register_handle:
+	kfree(handle);
+	return rc;
+}
+
+static bool input_filter(struct input_handle *handle, unsigned int type,
+		unsigned int code, int value)
+{
+	struct fpc1020_data *fpc1020 = handle->private;
+
+	if (type != EV_KEY)
+		return false;
+
+	return !fpc1020->report_keys;
+}
+
+static void input_disconnect(struct input_handle *handle)
+{
+	input_close_device(handle);
+	input_unregister_handle(handle);
+	kfree(handle);
+}
+
+static const struct input_device_id ids[] = {
+	{ .driver_info = 1 },
+	{ },
 };
 
 #ifdef CONFIG_FB
@@ -236,6 +300,32 @@ static ssize_t irq_ack(struct device *device,
 }
 static DEVICE_ATTR(irq, S_IRUSR | S_IWUSR, irq_get, irq_ack);
 
+static ssize_t enable_key_events_show(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+	struct fpc1020_data *fpc1020 = dev_get_drvdata(dev);
+
+	return sprintf(buf, "%d\n", fpc1020->report_keys);
+}
+
+static ssize_t enable_key_events_store(struct device *dev,
+	struct device_attribute *attr, const char *buf, size_t count)
+{
+	int rc;
+	unsigned long input;
+	struct fpc1020_data *fpc1020 = dev_get_drvdata(dev);
+
+	rc = kstrtoul(buf, 0, &input);
+	if (rc < 0)
+		return rc;
+
+	fpc1020->report_keys = !!input;
+
+	return count;
+}
+static DEVICE_ATTR(enable_key_events, S_IWUSR | S_IRUSR, enable_key_events_show,
+		   enable_key_events_store);
+
 static ssize_t enable_wakeup_show(struct device *dev,
 	struct device_attribute *attr, char *buf)
 {
@@ -245,7 +335,6 @@ static ssize_t enable_wakeup_show(struct device *dev,
 	c = fpc1020->wakeup_enabled ? '1' : '0';
 	return scnprintf(buf, PAGE_SIZE, "%c\n", c);
 }
-
 
 static ssize_t enable_wakeup_store(struct device *dev,
 	struct device_attribute *attr, const char *buf, size_t count)
@@ -306,6 +395,7 @@ static DEVICE_ATTR(proximity_state, S_IWUSR, NULL, proximity_state_set);
 
 static struct attribute *attributes[] = {
 	&dev_attr_irq.attr,
+	&dev_attr_enable_key_events.attr,
 	&dev_attr_enable_wakeup.attr,
 	&dev_attr_proximity_state.attr,
 	NULL
@@ -496,6 +586,19 @@ static int fpc1020_tee_probe(struct platform_device *pdev)
 	spin_lock_init(&fpc1020->irq_lock);
 	fpc1020->irq_enabled = true;
 
+	fpc1020->report_keys = false;
+
+	fpc1020->input_handler.filter = input_filter;
+	fpc1020->input_handler.connect = input_connect;
+	fpc1020->input_handler.disconnect = input_disconnect;
+	fpc1020->input_handler.name = FPC1020_NAME;
+	fpc1020->input_handler.id_table = ids;
+	rc = input_register_handler(&fpc1020->input_handler);
+	if (rc) {
+		dev_err(dev, "failed to register key handler\n");
+		goto exit;
+	}
+
 	rc = sysfs_create_group(&dev->kobj, &attribute_group);
 	if (rc) {
 		dev_err(dev, "could not create sysfs\n");
@@ -583,7 +686,7 @@ static const struct dev_pm_ops fpc1020_dev_pm_ops = {
 
 static struct platform_driver fpc1020_driver = {
 	.driver = {
-		.name = "fpc1020",
+		.name = FPC1020_NAME,
 		.owner = THIS_MODULE,
 		.of_match_table = fpc1020_of_match,
 #ifdef CONFIG_PM

mesziman/kernel_xiaomi_msm8996

drivers/input/fingerprint/fpc1020_tee.c
@@ -50,6 +50,8 @@
 #include <linux/fb.h>
 #endif
 
+#define FPC1020_NAME "fpc1020"
+
 #define FPC1020_RESET_LOW_US 1000
 #define FPC1020_RESET_HIGH1_US 100
 #define FPC1020_RESET_HIGH2_US 1250
@@ -74,6 +76,9 @@ struct fpc1020_data {
 
 	bool screen_on;
 	int proximity_state; /* 0:far 1:near */
+
+	struct input_handler input_handler;
+	bool report_key_events;
 };
 
 enum {
@@ -99,6 +104,65 @@ static void config_irq(struct fpc1020_data *fpc1020, bool enabled)
 			enabled ?  "true" : "false");
 	}
 }
+
+static int input_connect(struct input_handler *handler,
+		struct input_dev *dev, const struct input_device_id *id) {
+	int rc;
+	struct input_handle *handle;
+	struct fpc1020_data *fpc1020 =
+		container_of(handler, struct fpc1020_data, input_handler);
+
+	if (!strstr(dev->name, "uinput-fpc"))
+		return -ENODEV;
+
+	handle = kzalloc(sizeof(struct input_handle), GFP_KERNEL);
+	if (!handle)
+		return -ENOMEM;
+
+	handle->dev = dev;
+	handle->handler = handler;
+	handle->name = FPC1020_NAME;
+	handle->private = fpc1020;
+
+	rc = input_register_handle(handle);
+	if (rc)
+		goto err_input_register_handle;
+
+	rc = input_open_device(handle);
+	if (rc)
+		goto err_input_open_device;
+
+	return 0;
+
+err_input_open_device:
+	input_unregister_handle(handle);
+err_input_register_handle:
+	kfree(handle);
+	return rc;
+}
+
+static bool input_filter(struct input_handle *handle, unsigned int type,
+		unsigned int code, int value)
+{
+	struct fpc1020_data *fpc1020 = handle->private;
+
+	return !fpc1020->report_key_events;
+}
+
+static void input_disconnect(struct input_handle *handle)
+{
+	input_close_device(handle);
+	input_unregister_handle(handle);
+	kfree(handle);
+}
+
+static const struct input_device_id ids[] = {
+	{
+		.flags = INPUT_DEVICE_ID_MATCH_EVBIT,
+		.evbit = { BIT_MASK(EV_KEY) },
+	},
+	{ },
+};
 
 #ifdef CONFIG_FB
 static int fpc1020_fb_notifier_cb(struct notifier_block *self,
@@ -242,6 +306,32 @@ static ssize_t irq_ack(struct device *device,
 }
 static DEVICE_ATTR(irq, S_IRUSR | S_IWUSR, irq_get, irq_ack);
 
+static ssize_t enable_key_events_show(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+	struct fpc1020_data *fpc1020 = dev_get_drvdata(dev);
+
+	return sprintf(buf, "%d\n", fpc1020->report_key_events);
+}
+
+static ssize_t enable_key_events_store(struct device *dev,
+	struct device_attribute *attr, const char *buf, size_t count)
+{
+	int rc;
+	unsigned long input;
+	struct fpc1020_data *fpc1020 = dev_get_drvdata(dev);
+
+	rc = kstrtoul(buf, 0, &input);
+	if (rc < 0)
+		return rc;
+
+	fpc1020->report_key_events = !!input;
+
+	return count;
+}
+static DEVICE_ATTR(enable_key_events, S_IWUSR | S_IRUSR, enable_key_events_show,
+		   enable_key_events_store);
+
 static ssize_t enable_wakeup_show(struct device *dev,
 	struct device_attribute *attr, char *buf)
 {
@@ -300,6 +390,7 @@ static DEVICE_ATTR(proximity_state, S_IWUSR, NULL, proximity_state_set);
 
 static struct attribute *attributes[] = {
 	&dev_attr_irq.attr,
+	&dev_attr_enable_key_events.attr,
 	&dev_attr_enable_wakeup.attr,
 	&dev_attr_proximity_state.attr,
 	NULL
@@ -484,6 +575,19 @@ static int fpc1020_tee_probe(struct platform_device *pdev)
 	}
 	fpc1020->irq_enabled = true;
 
+	fpc1020->report_key_events = false;
+
+	fpc1020->input_handler.filter = input_filter;
+	fpc1020->input_handler.connect = input_connect;
+	fpc1020->input_handler.disconnect = input_disconnect;
+	fpc1020->input_handler.name = FPC1020_NAME;
+	fpc1020->input_handler.id_table = ids;
+	rc = input_register_handler(&fpc1020->input_handler);
+	if (rc) {
+		dev_err(dev, "failed to register key handler\n");
+		goto exit;
+	}
+
 	rc = sysfs_create_group(&dev->kobj, &attribute_group);
 	if (rc) {
 		dev_err(dev, "could not create sysfs\n");
@@ -539,7 +643,7 @@ static const struct dev_pm_ops fpc1020_dev_pm_ops = {
 
 static struct platform_driver fpc1020_driver = {
 	.driver = {
-		.name = "fpc1020",
+		.name = FPC1020_NAME,
 		.owner = THIS_MODULE,
 		.of_match_table = fpc1020_of_match,
 #ifdef CONFIG_PM