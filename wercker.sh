1d34e9a5fe785d370412a49bbbcc7fcdeb82073e
Gabor Meszaros <gabor.meszaros@nokia.com>
2019-05-23 11:50:56 +0000

    Revert "use 4.9 for 32"
    This reverts commit 3fcfa08cffa3770d83f919d18655b545b43d3df4.

--- wercker.sh
+++ wercker.sh
@@ -5,8 +5,8 @@ dpkg --add-architecture amd64 && apt-get -qq update && apt-get -qq install -y li
 export LOFASZ=$PWD && 
 #git clone --depth=10 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 /pipeline/build/root/toolchain/aarch64-linux-android-4.9 &&
 #cd /pipeline/build/root/toolchain/aarch64-linux-android-4.9 && git reset --hard 22f053ccdfd0d73aafcceff3419a5fe3c01e878b &&
-git clone --depth=10 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 /pipeline/build/root/toolchain/arm-linux-androideabi-4.9 &&
-cd /pipeline/build/root/toolchain/arm-linux-androideabi-4.9 && git reset --hard 42e5864a7d23921858ca8541d52028ff88acb2b6 
+#git clone --depth=10 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 /pipeline/build/root/toolchain/arm-linux-androideabi-4.9 &&
+#cd /pipeline/build/root/toolchain/arm-linux-androideabi-4.9 && git reset --hard 42e5864a7d23921858ca8541d52028ff88acb2b6 
 #git clone --depth=2 https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 /pipeline/build/root/toolchain/gclang &&
 #git clone --depth=1 -b 6.0.9 https://github.com/nvertigo/llvm-Snapdragon_LLVM_for_Android /pipeline/build/root/toolchain/SnapDragonLLVM_6.0/prebuilt/linux-x86_64/
 #git clone --depth=2 -b 9.0 https://github.com/syberia-project/platform_prebuilts_build-tools /pipeline/build/root/toolchain/asd
