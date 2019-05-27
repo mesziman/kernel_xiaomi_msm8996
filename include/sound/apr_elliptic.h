93e3fdc6a04fd71e121147f1293eb7ec2ac1d2c2
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2019-05-27 20:32:28 +0200

    Revert "sound: elliptic: adapt for new afe driver"
    This reverts commit 2ad9225e2aeb335c8da563dbb3ab016b7e0dbf1c.

--- include/sound/apr_elliptic.h
+++ include/sound/apr_elliptic.h
@@ -58,9 +58,22 @@ struct afe_ultrasound_set_params_t {
 	uint32_t  payload[ELLIPTIC_SET_PARAMS_SIZE];
 } __packed;
 
+struct afe_ultrasound_config_command {
+	struct apr_hdr                      hdr;
+	struct afe_port_cmd_set_param_v2    param;
+	struct afe_port_param_data_v2       pdata;
+	struct afe_ultrasound_set_params_t  prot_config;
+} __packed;
+
 /** Sequence of Elliptic Labs Ultrasound module parameters */
 struct afe_ultrasound_get_params_t {
 	uint32_t payload[ELLIPTIC_GET_PARAMS_SIZE];
+} __packed;
+
+struct afe_ultrasound_get_calib {
+	struct afe_port_cmd_get_param_v2   get_param;
+	struct afe_port_param_data_v2      pdata;
+	struct afe_ultrasound_get_params_t res_cfg;
 } __packed;
 
 struct afe_ultrasound_calib_get_resp {
@@ -82,7 +95,3 @@ int afe_ultrasound_set_calib_data(int port,
 			  struct afe_ultrasound_set_params_t *prot_config,
 			  uint32_t length);
 
-int32_t ultrasound_apr_set(int32_t port_id, uint32_t *param_id,
-	u8 *user_params, int32_t length);
-
-int32_t elliptic_process_apr_payload(uint32_t *payload);
